611713c881f69cb06afb8700cf36ddb5cf3fa35d
mustafaalpyanikoglu <malpyanikoglu@gmail.com>
2024-06-19 12:18:56 +0300

    completed part one

--- fastApiProject/main.py
+++ fastApiProject/main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@app.get("/", description="This is our first route.", deprecated=True)
+async def base_get_route():
     return {"message": "Hello World"}
 
 
